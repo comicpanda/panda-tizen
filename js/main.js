41b10bfc348169bee630c6dc2b93cb4a92b5b0b6
soohong <shm119@tapasmedia.co>
2013-08-27 22:58:52 -0700

    add back button

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ var userAgent = "Panda Tizen mobile (iPhone;)";
 //Initialize function
 var init = function () {
     tizen.websetting.setUserAgentString(userAgent, successCallback);
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
-            tizen.application.getCurrentApplication().exit();
-    });
+   // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) { // assign hardware buttons click event handler
+		if(e.keyName == 'back') { // if back button is clicked
+			if (document.URL.indexOf("index.html") > 0) {
+				tizen.application.getCurrentApplication().exit();
+				} 
+			else {
+				history.back();
+			}
+		}
+		else if (e.keyName === 'menu') {
+		    // Handle menu options here
+         }
+	});
 };
 
 function successCallback()
