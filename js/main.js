cb496584347f8a083940ec071b4fd42ae00d7edd
soohong <shm119@tapasmedia.co>
2013-12-11 01:08:27 -0800

    add iframe on tizen

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 //Initialize function
 var init = function () {
-	
 	var userAgent = 'Panda Tizen mobile like iPhone - ' + navigator.userAgent; 
 	tizen.websetting.setUserAgentString(userAgent, successCallback);
+	
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if(e.keyName == "back") {
+            tizen.application.getCurrentApplication().exit();
+        }
+    });
 };
 
-function successCallback() {
-	window.location="http://m.tapastic.com/";
-}
+var successCallback = function() {
+	document.getElementById("body").innerHTML = '<iframe seamless src="http://m.tapastic.com" align="center" width="100%" height="100%"  frameborder=no scrolling="yes" id="pandaFrame"></iframe>'
+};
 
 window.onload = init;
