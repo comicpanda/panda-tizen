f86429d4d24c7c2453fd6508d45b025aa3329f27
soohong <shm119@tapasmedia.co>
2013-12-17 18:28:44 -0800

    fix url

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var init = function () {
 
 var successCallback = function() {
 	// add eventListener for tizenhwkey
-	document.getElementById("body").innerHTML = '<iframe sandbox="allow-scripts allow-forms" src="http://mlocal.tapastic.com" align="center" width="100%" height="100%"  frameborder=no scrolling="yes" id="pandaFrame"></iframe>';
+	document.getElementById("body").innerHTML = '<iframe sandbox="allow-scripts allow-forms" src="http://m.tapastic.com" align="center" width="100%" height="100%"  frameborder=no scrolling="yes" id="pandaFrame"></iframe>';
 	var pandaFrame = document.getElementById('pandaFrame'),
-		pandaFrameWin = pandaFrame.contentWindow || pandaFrame;
+	pandaFrameWin = pandaFrame.contentWindow || pandaFrame;
     document.addEventListener('tizenhwkey', function(e) {
     	if(e.keyName == "back") {
     		pandaFrameWin.location.pathname !== '/'?
