bfd5cf006aab96f795603c601fe45e0391b4611a
soohong <shm119@tapasmedia.co>
2013-12-17 18:22:05 -0800

    fix login problem

--- js/main.js
+++ js/main.js
@@ -2,17 +2,20 @@
 var init = function () {
 	var userAgent = 'Panda Tizen mobile like iPhone - ' + navigator.userAgent; 
 	tizen.websetting.setUserAgentString(userAgent, successCallback);
-	
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back") {
-            tizen.application.getCurrentApplication().exit();
-        }
-    });
 };
 
 var successCallback = function() {
-	document.getElementById("body").innerHTML = '<iframe seamless src="http://m.tapastic.com" align="center" width="100%" height="100%"  frameborder=no scrolling="yes" id="pandaFrame"></iframe>'
+	// add eventListener for tizenhwkey
+	document.getElementById("body").innerHTML = '<iframe sandbox="allow-scripts allow-forms" src="http://mlocal.tapastic.com" align="center" width="100%" height="100%"  frameborder=no scrolling="yes" id="pandaFrame"></iframe>';
+	var pandaFrame = document.getElementById('pandaFrame'),
+		pandaFrameWin = pandaFrame.contentWindow || pandaFrame;
+    document.addEventListener('tizenhwkey', function(e) {
+    	if(e.keyName == "back") {
+    		pandaFrameWin.location.pathname !== '/'?
+    		pandaFrameWin.history.back():
+            tizen.application.getCurrentApplication().exit();
+        }
+    });
 };
 
 window.onload = init;
